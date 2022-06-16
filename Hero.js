7474466699af1a7c4303145a7e8ba44f9cd14fdf
Lukasiku <semel015@hsd.k12.or.us>
2022-06-16 09:23:56 -0700

    adding replit files and touching up

---
+++ Hero.js
@@ -0,0 +1,33 @@
+var mobileBreakpoint = 600;
+
+window.onload = function() {
+    var screenWidth = window.innerWidth;
+    var globalNavbar = document.getElementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
+
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+        toggle.classList.add("open");
+    }
+
+    toggle.onclick = function() {
+        globalNavbar.classList.toggle("open");
+        toggle.classList.toggle("open");
+    }
+}
+
+
+window.onresize = function() {
+    var screenWidth = window.innerWidth;
+    var globalNavbar = document.getElementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
+
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+        toggle.classList.add("open");
+    }
+    else {
+        globalNavbar.classList.remove("open");
+        toggle.classList.remove("open");
+    }
+}
